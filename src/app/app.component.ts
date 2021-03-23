2cc0bd7b06eea7054fae23d8eea25c563c63c8c1
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-03-24 00:11:32 +0200

    Component details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basic';
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular', text: 'Я все еще учу компоненты', id: 1},
+    {title: 'Cледующий блок', text: 'Будет про дерективы и про пайпы', id: 2},
+    {title: 'Хочу выучить Angular', text: 'Я все еще учу компоненты', id: 3}
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+  }
 }
---
+++ src/app/post-form/post-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Post} from '../app.component';
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.scss']
+})
+export class PostFormComponent implements OnInit {
+
+  // Если нужно сделать данные на выход, в другой компонент например
+  @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
+
+  // static false, static true - если в ngOnInit
+  @ViewChild('titleInput', {static: false}) inputRef: ElementRef;
+
+  title = '';
+  text = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addPost() {
+    if (this.text.trim() && this.title.trim()) {
+      const post: Post = {
+        title: this.title,
+        text: this.text
+      };
+
+      this.onAdd.emit(post);
+
+      this.title = this.text = '';
+    }
+  }
+
+  focusTitle() {
+    this.inputRef.nativeElement.focus();
+  }
+
+}
