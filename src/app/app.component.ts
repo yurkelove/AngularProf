65837b5a49aeb3c5fb3c287338d51087acda266e
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-04-05 22:19:47 +0300

    Form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {MyValidators} from './my.validators';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,57 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        MyValidators.restrictedEmails],
+        [MyValidators.uniqEmail]
+      ),
+      password: new FormControl(null,
+        [Validators.required,
+        Validators.minLength(6)
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required)
+      }),
+      skills: new FormArray([])
+    });
+  }
+
+  submitForm() {
+    if ( this.form.valid) {
+      console.log('Form submitted: ', this.form);
+      const form = {...this.form.value};
+
+      console.log('Form data', form);
+
+      this.form.reset();
+    }
+  }
+
+  setCapital() {
+    const cityMap = {
+      ru: 'Москва',
+      ua: 'Киев',
+      by: 'Минск'
+    };
+
+    const cityKey = this.form.get('address').get('country').value;
+    const city = cityMap[cityKey];
+
+    this.form.patchValue({address: {city: city}});
+  }
+
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+
+    (this.form.get('skills') as FormArray).push(control);
+  }
 
 }
