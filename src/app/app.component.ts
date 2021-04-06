44ea305fff2bf108a05d50c9aaaecbc311dc800f
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-04-06 22:55:09 +0300

    Start learn http-client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
+import {Todo, TodoService} from './todo.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,57 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = [];
+
+  todoTitle = '';
+
+  loading = false;
+
+  constructor(private todosService: TodoService) {
+
+  }
+
+  ngOnInit() {
+   this.fetchTodos();
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) {
+      return;
+    }
+
+    this.todosService.addTodo({
+      title: this.todoTitle,
+      completed: false
+    }) .subscribe(todo => {
+      console.log(todo);
+      this.todos.push(todo);
+      this.todoTitle = '';
+    });
+  }
+
+  fetchTodos() {
+    this.loading = true;
+    this.todosService.fetchTodos()
+      .subscribe(response => {
+        this.todos = response;
+        this.loading = false;
+      });
+  }
+
+  removeTodo(id: number) {
+      this.todosService.removeTodos(id)
+      .subscribe(response => {
+        this.todos = this.todos.filter(t => t.id !== id);
+      });
+  }
+
+  completeTodo(id: number) {
+    this.todosService.completeTodo(id).subscribe(todo => {
+      this.todos.find(t => t.id === todo.id).completed = true;
+    });
+  }
 
 }
---
+++ src/app/todo.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {delay} from 'rxjs/operators';
+
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number;
+}
+
+@Injectable({providedIn: 'root'})
+
+export class TodoService {
+  constructor(private http: HttpClient) {
+
+  }
+
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo);
+  }
+
+  fetchTodos():Observable<Todo[]> {
+    return  this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay(1500));
+  }
+
+  removeTodos(id: number): Observable<void> {
+    return  this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  }
+
+  completeTodo(id:number): Observable<Todo> {
+    return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      completed: true
+    });
+  }
+
+}
