6dc1ac50813e00ceedb239599321556cd4042969
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-05-20 11:28:52 +0300

    Routing without query

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core'
+import {Injectable} from '@angular/core';
 
 export interface Post {
-  title: string
-  text: string
-  id: number
+  title: string;
+  text: string;
+  id: number;
 }
 
 @Injectable({providedIn: 'root'})
@@ -13,9 +13,9 @@ export class PostsService {
     {title: 'Post 2', text: 'Sample text for post 2', id: 22},
     {title: 'Post 3', text: 'Sample text for post 3', id: 33},
     {title: 'Post 4', text: 'Sample text for post 4', id: 44},
-  ]
+  ];
 
   getById(id: number) {
-    return this.posts.find(p => p.id === id)
+    return this.posts.find(p => p.id === id);
   }
 }
