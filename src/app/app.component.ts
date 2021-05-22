e6644269f06271e0bfde291545656d0f0ed17c38
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-05-22 21:20:43 +0300

    Add dynamic component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core'
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {ModalComponent} from './modal/modal.component';
+import {RefDirective} from './ref.directive';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +8,21 @@ import {Component} from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  modal = false
+
+  @ViewChild(RefDirective, {static: false}) refDir: RefDirective;
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
+    this.refDir.containerRef.clear();
+
+    const component = this.refDir.containerRef.createComponent(modalFactory);
+    component.instance.title = 'Dynamic title';
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear();
+    });
+  }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component'
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { ModalComponent } from './modal/modal.component'
+import { ModalComponent } from './modal/modal.component';
+import {RefDirective} from './ref.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalComponent,
+    RefDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
   providers: [],
+  entryComponents: [ModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {
