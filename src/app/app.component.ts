60a2ca5b578b474703ab8ba4f7f8c67f18fd6eed
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-05-22 21:28:47 +0300

    Add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
 import {ModalComponent} from './modal/modal.component';
 import {RefDirective} from './ref.directive';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,18 @@ export class AppComponent {
 
   @ViewChild(RefDirective, {static: false}) refDir: RefDirective;
 
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private title: Title,
+    private meta: Meta
+  ) {
+    const t = title.getTitle();
+    title.setTitle('AppComponentPage');
+    this.meta.addTags([
+      { name: 'keywords', content: 'angular, google, appcomponent'},
+      { name: 'description', content: 'this is app component'}
+    ]);
+  }
 
   showModal() {
     const modalFactory = this.resolver.resolveComponentFactory(ModalComponent);
