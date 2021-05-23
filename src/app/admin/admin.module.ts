c49ef7b66960a3e8580cfbef6856643f4193f9c2
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-05-23 23:32:37 +0300

    Admin panel lesson 1, with error in validation(not debugg)

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,11 +7,14 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [ComponentsComponent, AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([
       {
         path: '', component: AdminLayoutComponent, children: [
