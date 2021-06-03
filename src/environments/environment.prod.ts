09fec8625478af053426e1a9313392917ee272bc
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-06-03 18:29:35 +0300

    Add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyC53TrQRy9kHUwa_pZkA7-KQOfP2hhuL14'
 };
