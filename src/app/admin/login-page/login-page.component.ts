ad681e6e7115b91ba1d49fb11608c9380fc23333
Yura Kuzkin <y.kuzkin@sendpulse.com>
2021-06-03 20:24:02 +0300

    Add auth guard, fix admin layout, start create-page

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,13 +12,22 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted: boolean;
+  message: string;
 
   constructor(
-    private auth: AuthService,
-    private router: Router
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['loginAgain']) {
+        this.message = 'Пожалуйста, введите данные';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
@@ -30,6 +39,8 @@ export class LoginPageComponent implements OnInit {
       return ;
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -38,6 +49,9 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
     });
   }
 
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,35 +1,72 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FbAuthResponse, User} from '../../../shared/interfaces';
-import {Observable} from 'rxjs';
+import {Observable, Subject, throwError} from 'rxjs';
 import {environment} from '../../../../environments/environment';
-import {tap} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 
 export class AuthService {
+
+  public  error$: Subject<string> = new Subject<string>();
+
  constructor(private http: HttpClient) {}
 
   get token(): string {
-   return '';
+   const expDate = new Date(localStorage.getItem('fb-token-exp'));
+   if (new Date() > expDate) {
+     this.logout();
+     return null;
+   }
+   return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+   user.returnSecureToken = true;
    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
      .pipe(
-      tap(this.setToken)
+      tap(this.setToken),
+      catchError(this.handleError.bind(this))
      );
   }
 
   logout() {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse) {
+  private handleError(error: HttpErrorResponse) {
+    const { message } = error.error.error;
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный email');
+        break;
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль');
+        break;
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Такого email нет');
+        break;
+    }
+
+    console.log(message);
+
+    return throwError(error);
+  }
+
+  private setToken(response: FbAuthResponse | null) {
+      if (response) {
+        const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+        localStorage.setItem('fb-token', response.idToken);
+        localStorage.setItem('fb-token-exp', expDate.toString());
+      } else {
+        localStorage.clear();
+      }
 
   }
 }
